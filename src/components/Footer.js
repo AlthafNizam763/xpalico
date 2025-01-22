36132ea71b6feefd446fdbd6ba95c7d66d08fefb
althaf <althaf@gmail.com>
2025-01-22 11:42:32 +0530

    BABABAAAA

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import rightpic from '../assets/Miky.png'; 
-import leftpic from '../assets/Minato.png'; 
+// import rightpic from '../assets/Miky.png'; 
+// import leftpic from '../assets/Minato.png'; 
+
 
 const FooterContainer = styled.footer`
   padding: 20px;
@@ -11,27 +12,27 @@ const FooterContainer = styled.footer`
   position: relative;
 `;
 
-const Image = styled.img`
-  position: absolute;
-  width: 100px; // Adjust the width as needed
-`;
+// const Image = styled.img`
+//   position: absolute;
+//   width: 100px; // Adjust the width as needed
+// `;
 
-const RightImage = styled(Image)`
-  right: 1px;
-  bottom: 1px;
-`;
+// const RightImage = styled(Image)`
+//   right: 1px;
+//   bottom: 1px;
+// `;
 
-const LeftImage = styled(Image)`
-  left: 1px;
-  bottom: 1px;
-`;
+// const LeftImage = styled(Image)`
+//   left: 1px;
+//   bottom: 1px;
+// `;
 
 const Footer = () => {
   return (
     <FooterContainer>
       <p>&copy; 2024 My Website. All Rights Reserved.</p>
-      <RightImage src={rightpic} alt="Right Side Image" />
-      <LeftImage src={leftpic} alt="Left Side Image" />
+      {/* <RightImage src={rightpic} alt="Right Side Image" />
+      <LeftImage src={leftpic} alt="Left Side Image" /> */}
     </FooterContainer>
   );
 };
